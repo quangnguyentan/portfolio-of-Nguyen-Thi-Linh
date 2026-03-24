013d039a5e108d60a4b19c59f06959cec2b275b5
Cael Nguyen <cael.nguyen@avepoint.com>
2026-03-24 21:58:13 +0700

    first commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Github from "./Github";
-import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
@@ -39,8 +38,6 @@ function About() {
           <h1 className="project-heading">
             Professional <strong className="purple">Skillset </strong>
           </h1>
-
-          {/* <Techstack /> */}
 
           <h1 className="project-heading">
             <strong className="purple">Tools</strong> I use
